250ca840c19e0eb3dd5a888287a6caf29f48c289
Aftab-Developer <AftabKhanDev@gmail.com>
2025-11-26 15:44:44 +0500

    added

--- config/gemeniConfig.js
+++ config/gemeniConfig.js
@@ -2,6 +2,9 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const {GoogleGenerativeAI}  = require('@google/generative-ai'); 
+console.log(process.env.GEMINI_API_KEY || "null");
+
+
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); 
 const model = genAI.getGenerativeModel({model:"gemini-2.0-flash"}); 
