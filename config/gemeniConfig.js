d318a0ae55a96dd1ad3f315442572f4fbcded5e9
Mohammad Aftab <AftabKhanDev@gmail.com>
2025-12-09 14:01:42 +0500

    model name change

--- config/gemeniConfig.js
+++ config/gemeniConfig.js
@@ -7,6 +7,6 @@ console.log(process.env.GEMINI_API_KEY || "null");
 
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); 
-const model = genAI.getGenerativeModel({model:"gemini-2.0-flash"}); 
+const model = genAI.getGenerativeModel({model:"gemini-1.5-flash"}); 
 
 module.exports = model
